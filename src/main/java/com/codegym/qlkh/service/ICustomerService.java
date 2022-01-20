dd88a82d216c43a8a66c46a33ca1d34db1a347d5
Admin <tran.x.dat1996@gmail.com>
2022-01-20 09:37:01 +0700

    First commit

---
+++ src/main/java/com/codegym/qlkh/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.codegym.qlkh.service;
+
+
+import com.codegym.qlkh.model.Customer;
+import com.codegym.qlkh.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
